fb5ccaa4264ecf41bad6587edba03164518f69d2
chengbotao <t221007s@163.com>
2020-11-04 15:01:54 +0800

    chore(commit-message rules): add commit message rules
    add commit message rules

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: Chengbotao
+ * @Date: 2020-11-04 11:00:35
+ * @LastEditors: Chengbotao
+ * @LastEditTime: 2020-11-04 11:02:11
+ * @FilePath: \bee-design-vue\rollup.config.js
+ */
+
+export default {
+  
+}
